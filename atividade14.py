c423c1f8356d82b2d6be6574a16605e53d706020
maria-angelina <maria.angelina17492@gmail.com>
2025-10-02 13:10:15 -0300

    first commit

---
+++ atividade14.py
@@ -0,0 +1,5 @@
+# Crie uma tupla com os dias da semana e exiba o terceiro e o sexto dia.
+dias= ("segunda","terça","quarta","quinta","sexta","sabado","domingo")
+
+print("terceiro dia:",dias[2])
+print("sexto dia:",dias[5])
